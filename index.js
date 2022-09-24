cd0f921e1c3d64d88c906ff4c08797ac0ab21fdf
Aaron <ztoplin@yahoo.com>
2022-09-24 17:24:35 -0400

    commit tree 780a48b662aef339a12d231f4ee6c2fb0675bb5f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(guestNames, eventName) {
+    let cards = []
+    for (let i = 0; i < guestNames.length; i++) {
+        cards.push(`Thank you, ${guestNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+     countDown(guestNames.length);
+    return cards
+}
+
+function countDown(number) {
+    let count = number;
+    while(count >= 0) {
+      console.log(count--);
+    }
+  };
+  
+
